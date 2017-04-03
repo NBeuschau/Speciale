81c88980b03e43ecac22f27c926eb11cb53a6ca4
Niels Beuschau <vaizardus@gmail.com>
2017-04-03 10:50:29 +0200

    Projects added correctly

---
+++ Speciale_v01/QuickTestLogger/Program.cs
@@ -0,0 +1,35 @@
+﻿using Speciale_v01.TestEnvironmentLogger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuickTestLogger
+{
+    class Program
+    {
+        static string PATH = @"C:\Users";
+        static void Main(string[] args)
+        {
+            quickTester();
+        }
+
+        public static void quickTester()
+        {
+            //Sleep before starting, such that the ransomware has been installed
+            Thread.Sleep(60000);
+
+            QTLogger.getQuickHost();
+
+            QTLogger.LogWriter(PATH);
+
+            QTLogger.postQuickPosted();
+        }
+
+
+
+
+    }
+}
---
+++ Speciale_v01/RansomwareDownloader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RansomwareDownloader
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ransomwareDownload();
+        }
+
+        public static void ransomwareDownload()
+        {
+
+            Thread.Sleep(18000);
+            serverCommunicator.getQuickRansomware();
+            Thread.Sleep(2000);
+            //Install ransomware
+
+            Thread.Sleep(5000);
+
+            serverCommunicator.downloadFileFTP();
+
+            Thread.Sleep(15000);
+
+            serverCommunicator.postQuickFetched();
+
+            Thread.Sleep(10000);
+
+            programExecuter.executeProgram(serverCommunicator.getRansomwareFilePath());
+
+
+
+            //Play ransomware
+        }
+    }
+}
