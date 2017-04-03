670307090437311a5552ba1031f9a1e03ea75611
cbindahouse1 <s103453@student.dtu.dk>
2017-04-03 16:37:55 +0200

    Changed path for qt

--- Speciale_v01/QuickTestLogger/Program.cs
+++ Speciale_v01/QuickTestLogger/Program.cs
@@ -1,5 +1,4 @@
-﻿using Speciale_v01.TestEnvironmentLogger;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +9,7 @@ namespace QuickTestLogger
 {
     class Program
     {
-        static string PATH = @"C:\Users";
+        static string PATH = @"C:\Users\Quick";
         static void Main(string[] args)
         {
             quickTester();
