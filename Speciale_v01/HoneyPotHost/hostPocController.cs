aadc1eb2a5be82e6dae21343f4fab1fd8208a7b7
Niels Beuschau <vaizardus@gmail.com>
2017-04-26 15:32:47 +0200

    HoneyPotHostController

---
+++ Speciale_v01/HoneyPotHost/hostPocController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HoneyPotHost
+{
+    class hostPocController
+    {
+        private static string nameOfMachine = "";
+        private static string nameofStartUpSnapshot = "";
+
+        //Hosts the baseline every 33 minute
+        public static void hostOfPOCTester()
+        {
+            //Creates a virtualmachine controller
+            VirtualMachineController tempVir = null;
+            while (true)
+            {
+                //Instances a new virtual machine
+                tempVir = new VirtualMachineController();
+
+                //Starts up the machine
+                tempVir.startVirtualMachine(nameOfMachine);
+                Thread.Sleep(1980000);
+
+                //Powers off the machine
+                tempVir.poweroffVirtualMachine(nameOfMachine);
+                Thread.Sleep(5000);
+
+                //Restores the virtual machine to the original image
+                tempVir.restoreVirtualMachine(nameOfMachine, nameofStartUpSnapshot);
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
