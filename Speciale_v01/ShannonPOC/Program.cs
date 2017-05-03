e784a486061c1fa125df347c9b9f3dbfaf5d5018
Niels Beuschau <vaizardus@gmail.com>
2017-05-03 18:03:09 +0200

    ShannonEntropy started

---
+++ Speciale_v01/ShannonPOC/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShannonPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\speciale\test\shannon\bitch1.7z";
+            FileInfo fil = new FileInfo(path);
+
+            Console.WriteLine(ShannonEntropy.CalculateEntropy(fil));
+            path = @"C:\speciale";
+            EntropyHandler nonStatic = new EntropyHandler();
+            Dictionary<string, double> temp = nonStatic.getEntropyOfAllFilesInPath(path);
+
+            foreach (var item in temp)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+
+            Thread.Sleep(20000);
+        }
+    }
+}
