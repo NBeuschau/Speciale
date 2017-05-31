dad15754aac058a02996f67b3360f404f3fd5fb6
Niels Beuschau <vaizardus@gmail.com>
2017-05-31 10:43:34 +0200

    Trying to access database

---
+++ Speciale_v01/DatabaseCollector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseCollector
+{
+    class Program
+    {
+        static string databaseinputbase = "http://192.168.8.102/v1/index.php/getdata";
+        static string databaseTester = "hp1";
+        static string middlepart = "?RansomwareName=";
+        static string ransomwareName = "Vipsana2";
+
+        private static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+          
+            var responseString = client.GetStringAsync(databaseinputbase+databaseTester+middlepart+ransomwareName).Result;
+
+            Console.WriteLine(responseString);
+            Console.ReadLine();
+        }
+
+
+    }
+}
+}
